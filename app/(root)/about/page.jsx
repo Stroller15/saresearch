d7e6cebd61f71a011aae86488da3c8df5452f37b
Stroller15 <shubhamverma6351@gmail.com>
2024-06-10 21:51:24 +0530

    add about us component

--- app/(root)/about/page.jsx
+++ app/(root)/about/page.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from 'react';
 
-const page = () => {
+const AboutUs = () => {
   return (
-    <div>about</div>
-  )
-}
+    <div className="flex  min-h-screen bg-gradient-to-r from-blue-800 to-indigo-900 p-6">
+      <div className="text-left max-w-3xl">
+        <h1 className="text-6xl font-bold mb-4 text-white">About Us</h1>
+        <p className="text-2xl text-white leading-relaxed">
+          S&A Research is your trusted partner in achieving your business goals and visions. Established with a commitment to excellence and a passion for helping businesses thrive, we specialize in providing comprehensive business consulting services to a diverse clientele, including startups, MSMEs, corporates, and MNCs.
+        </p>
+      </div>
+    </div>
+  );
+};
 
-export default page
+export default AboutUs;
--- app/(root)/services/page.jsx
+++ app/(root)/services/page.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Services = () => {
   return (
-    <div className="min-h-screen bg-gradient-to-r from-blue-500 to-blue-600 text-white">
+    <div className="min-h-screen bg-gradient-to-r from-blue-800 to-indigo-900 text-white">
       <div className="max-w-4xl mx-auto p-4">
         <div className="service mb-8">
           <h1 className='text-2xl font-semibold mb-5'>Our Services</h1>
