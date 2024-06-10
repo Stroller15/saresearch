8b6c07b322f077bb4222b9cc1c8c29de6f945080
Stroller15 <shubhamverma6351@gmail.com>
2024-06-10 21:19:23 +0530

    fix padding in banner

--- app/(root)/services/page.jsx
+++ app/(root)/services/page.jsx
@@ -2,22 +2,22 @@ import React from 'react';
 
 const Services = () => {
   return (
-    <div className="min-h-screen bg-[#001E5D] text-white">
+    <div className="min-h-screen bg-gradient-to-r from-blue-500 to-blue-600 text-white">
       <div className="max-w-4xl mx-auto p-4">
         <div className="service mb-8">
           <h1 className='text-2xl font-semibold mb-5'>Our Services</h1>
-          <h2 className="text-xl font-semibold">1 - Business strategy consultant</h2>
+          <h2 className="text-xl font-semibold">1 - Business Strategy Consultant</h2>
           <ul className="ml-4">
-            <li>a. Acquisition advisory</li>
+            <li>a. Acquisition Advisory</li>
             <li>b. Capex advisory</li>
             <li>c. Growth Advisory</li>
             <li>d. Entry strategy</li>
           </ul>
         </div>
         <div className="service mb-8">
-          <h2 className="text-xl font-semibold">2 - Industry research</h2>
+          <h2 className="text-xl font-semibold">2 - Industry Research</h2>
           <ul className="ml-4">
-            <li>a. Market assessment</li>
+            <li>a. Market Assessment</li>
             <li>b. Company profiling</li>
             <li>c. International benchmarking</li>
             <li>d. Economic research</li>
@@ -25,7 +25,7 @@ const Services = () => {
           </ul>
         </div>
         <div className="service mb-8">
-          <h2 className="text-xl font-semibold">3 - Investment research</h2>
+          <h2 className="text-xl font-semibold">3 - Investment Research</h2>
           <ul className="ml-4">
             <li>a. Financial modelling</li>
             <li>b. Pitch Deck creation</li>
