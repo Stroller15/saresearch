80e6dcf6b07901d14688a693f8f9f6a2c62ffdf0
Stroller15 <shubhamverma6351@gmail.com>
2024-06-11 17:59:56 +0530

    add background image

--- app/(root)/about/page.jsx
+++ app/(root)/about/page.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 const AboutUs = () => {
   return (
-    <div className="flex  min-h-screen bg-gradient-to-r from-blue-800 to-indigo-900 p-6">
+    <div className="flex min-h-screen bg-[url('/bg-building.jpeg')] bg-cover bg-no-repeat p-6">
       <div className="text-left max-w-3xl">
-        <h1 className="text-6xl font-bold mb-4 text-white">About Us</h1>
-        <p className="text-2xl text-white leading-relaxed">
+        <h1 className="text-6xl font-bold mb-4 text-black">About Us</h1>
+        <p className="sm:text-2xl text-black sm:font-bold sm:leading-loose leading-8 font-semibold">
           S&A Research is your trusted partner in achieving your business goals and visions. Established with a commitment to excellence and a passion for helping businesses thrive, we specialize in providing comprehensive business consulting services to a diverse clientele, including startups, MSMEs, corporates, and MNCs.
         </p>
       </div>
--- app/(root)/services/page.jsx
+++ app/(root)/services/page.jsx
@@ -2,21 +2,21 @@ import React from 'react';
 
 const Services = () => {
   return (
-    <div className="min-h-screen bg-gradient-to-r from-blue-800 to-indigo-900 text-white">
-      <div className="max-w-4xl mx-auto p-4">
+    <div className="min-h-screen bg-[url('/bg-building.jpeg')] bg-cover bg-no-repeat   text-white">
+      <div className="max-w-4xl  sm:p-8 sm:ml-10 p-3">
         <div className="service mb-8">
-          <h1 className='text-2xl font-semibold mb-5'>Our Services</h1>
-          <h2 className="text-xl font-semibold">1 - Business Strategy Consultant</h2>
-          <ul className="ml-4">
+          <h1 className='sm:text-4xl text-2xl font-bold sm:tracking-widest mb-5 text-black'>Our Services</h1>
+          <h2 className="text-xl font-bold sm:tracking-widest text-black">1 - Business Strategy Consultant</h2>
+          <ul className="ml-4  font-semibold sm:tracking-widest text-black">
             <li>a. Acquisition Advisory</li>
             <li>b. Capex advisory</li>
             <li>c. Growth Advisory</li>
             <li>d. Entry strategy</li>
           </ul>
         </div>
-        <div className="service mb-8">
-          <h2 className="text-xl font-semibold">2 - Industry Research</h2>
-          <ul className="ml-4">
+        <div className="service mb-8 sm:tracking-widest text-black">
+          <h2 className="text-xl sm:tracking-widest font-bold text-black">2 - Industry Research</h2>
+          <ul className="ml-4 font-semibold sm:tracking-widest text-black">
             <li>a. Market Assessment</li>
             <li>b. Company profiling</li>
             <li>c. International benchmarking</li>
@@ -24,16 +24,16 @@ const Services = () => {
             <li>e. Policy and Regulatory Research</li>
           </ul>
         </div>
-        <div className="service mb-8">
-          <h2 className="text-xl font-semibold">3 - Investment Research</h2>
-          <ul className="ml-4">
+        <div className="service mb-8 text-black">
+          <h2 className="text-xl sm:tracking-widest font-bold text-black">3 - Investment Research</h2>
+          <ul className="ml-4 font-semibold sm:tracking-widest text-black">
             <li>a. Financial modelling</li>
             <li>b. Pitch Deck creation</li>
             <li>c. Transaction Analysis</li>
           </ul>
         </div>
         <div className="service">
-          <h2 className="text-xl font-semibold">4 - Business Valuation</h2>
+          <h2 className="text-xl sm:tracking-widest text-black font-bold">4 - Business Valuation</h2>
         </div>
       </div>
     </div>
