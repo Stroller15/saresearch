13a9a83fd1903fa6f4e6dcc8ce77b2e42fd675e9
Stroller15 <shubhamverma6351@gmail.com>
2024-06-11 18:28:59 +0530

    fix bug

--- app/(root)/page.jsx
+++ app/(root)/page.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import Banner from "@/components/Banner";
-import Navbar from "@/components/Navbar";
+import Banner from "../../components/Banner";
+import Navbar from '../../components/Navbar';
 
 const Page = () => {
   return (
